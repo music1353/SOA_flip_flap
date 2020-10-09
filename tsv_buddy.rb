80a432d174745b6d71d86224e607dbfc9c92bf08
music1353 <music1353@gmail.com>
2020-10-09 22:23:25 +0800

    Solved the homework and all tests pass

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,31 @@
+# frozen_string_literal: true
+
+require 'csv'
+require 'yaml'
+
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
+    rows = tsv.split("\n").map { |line| line.split("\t") }
+    header = rows[0]
+    records = rows[1..-1]
+    @data = records.map do |record|
+      record.map.with_index { |item, idx| [header[idx], item] }.to_h
+    end
 
+    print @data
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
+    raise 'ERROR: please load data' if @data.nil?
 
+    keys_array = @data.first.keys
+    records = @data.map { |record| record.values.join("\t") }
+
+    ([keys_array.join("\t")] + records).join("\n") + "\n"
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,16 @@
+# frozen_string_literal: true
+
+require 'yaml'
+
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  def take_yaml(yaml)
+    @data = YAML.safe_load(yaml)
+  end
+
+  def to_yaml
+    raise 'ERROR: please load data' if @data.nil?
+
+    @data.to_yaml
+  end
+end
